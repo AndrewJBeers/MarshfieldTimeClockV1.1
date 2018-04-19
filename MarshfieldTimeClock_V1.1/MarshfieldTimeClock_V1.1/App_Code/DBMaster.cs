b1de9c3f0c1ae6e12b8520eacd7d7ec283640472
AndrewJBeers <31416788+AndrewJBeers@users.noreply.github.com>
2018-04-19 13:50:00 -0500

    Add files via upload

---
+++ MarshfieldTimeClock_V1.1/MarshfieldTimeClock_V1.1/App_Code/DBMaster.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace MarshfieldTimeClock_V1._1.App_Code
+{
+    public class DBMaster
+    {
+        
+        private SqlConnection conn;
+        public SqlConnection getConnection()
+        {
+            
+            //Connect to the db
+            string connStr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            conn = new SqlConnection(connStr);
+            conn.Open();
+            return conn;
+        }
+
+
+        public SqlDataReader getReader(string query)
+        {
+            // Create a command
+            //this connection string is not safe. Use a stored procedure
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.Connection = this.getConnection();
+
+            // read from db
+            SqlDataReader reader = cmd.ExecuteReader();
+            return reader;
+        }
+
+        public void closeConnection()
+        {
+            if (conn != null && conn.State == System.Data.ConnectionState.Open)
+            {
+                this.conn.Close();
+            }
+        }
+
+
+    }
+}
